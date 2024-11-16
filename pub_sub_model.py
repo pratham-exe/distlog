5c5f4b4cca4014d97cd235580afcb5df0b878684
Rahul <rahul.j.jacob@gmail.com>
2024-11-16 21:00:50 +0530

    added pub-sub model

--- pub_sub_model.py
+++ pub_sub_model.py
@@ -1,9 +1,49 @@
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, KafkaProducer
 import json
 import sys
+import time
+import threading
 
-topic_sub = sys.argv[1]
+topic_sub = "nodes"
 consumer = KafkaConsumer(topic_sub, value_deserializer = lambda m: json.loads(m.decode('ascii')))
+producer = KafkaProducer(value_serializer = lambda m: json.dumps(m).encode('ascii'))
+
+topic_pub_1 = "storage"
+topic_pub_2 = "alert"
+
+node_heartbeats = {}
+lock = threading.Lock()  # To manage concurrent access to shared data
+
+def monitor_heartbeats():
+    while True:
+        time.sleep(7)  # Check every 10 seconds
+        with lock:
+            current_time = time.time()
+            for node_id, last_heartbeat in list(node_heartbeats.items()):
+                if current_time - last_heartbeat > 7:
+                    print(f"ALERT: Node {node_id} missed its heartbeat!")
+
+monitor_thread = threading.Thread(target=monitor_heartbeats, daemon=True)
+monitor_thread.start()
+
+registered_nodes = []
 
 for message in consumer:
-    print(message.value)
+    node_id = message.value['node_id']
+    message_type = message.value['message_type']
+    if node_id not in registered_nodes:
+        if message_type == "REGISTRATION":
+            registered_nodes.insert(0, node_id)
+            node_heartbeats[node_id] = time.time()
+        else:
+            continue
+           
+    with lock:
+        if message_type == "HEARTBEAT":
+            node_heartbeats[node_id] = time.time()
+    
+    if message_type == "WARN" or message_type == "ERROR":
+        producer.send(topic_pub_2, message)
+        print(message)
+    
+    producer.send(topic_pub_1, message)
